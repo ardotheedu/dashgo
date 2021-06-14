5a4ee12a93da22970ba071a67a8859a27d095e36
ardotheedu <eduardosalima3@gmail.com>
2021-06-14 15:27:56 -0300

    feat: list users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,20 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users;
     })
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -64,19 +77,23 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Eduardo de Sá</Text>
-                                                <Text fontSize="sm" color="gray.300">eduardosalimaa@hotmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 de Abril, 2021</Td> }
-                                        
-                                    </Tr>
+                                   {data.map(user => {
+                                       return (
+                                        <Tr key={user.id}>
+                                            <Td px={["4", "4", "6"]}>
+                                                <Checkbox colorScheme="pink" />
+                                            </Td>
+                                            <Td>
+                                                <Box>
+                                                    <Text fontWeight="bold">{user.name}</Text>
+                                                    <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                </Box>
+                                            </Td>
+                                            {isWideVersion && <Td>{user.createdAt}</Td> }
+                                            
+                                        </Tr>
+                                       )
+                                   })}
                                 </Tbody>
                             </Table>
 
